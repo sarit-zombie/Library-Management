043e12d6d4315652a261f752e26d8f1fc8d9d682
sarit-zombie <57765371+sarit-zombie@users.noreply.github.com>
2020-11-06 13:41:02 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/librarymanagement/User_register.java
@@ -0,0 +1,14 @@
+package com.example.librarymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
